00be059243e96a812b6fb9e7dfa8b4a5be36f672
Dorian Houdelette <houdeled@ensipc613.ensimag.fr>
2020-01-17 15:43:04 +0100

    factorisation du code, on peut désormais appeler le script de test sur un seul programme

---
+++ src/test/script/testcodegenvalid.sh
@@ -0,0 +1,38 @@
+#! /bin/bash
+
+cd "$(dirname "$0")"/../../.. || exit 1
+PATH=./src/test/script/launchers:"$PATH"
+
+f=$1
+catPgm=$2
+catAss=$3
+
+echo -en "$f"
+a=${f:: -5}.ass
+decac $f > /dev/null
+if [[ $catPgm == "true" ]];
+then
+    echo
+    cat $f
+fi
+if [[ $catAss == "true" ]];
+then
+    echo
+    cat $a
+fi
+if [ ! -f $a ]; then
+    echo -e "\e[93mFichier .ass non généré."
+    echo -en "\e[39m"
+else
+    result=$(ima $a)
+    rm $a
+fi
+result=$(ima $a)
+expresult=$(grep @result $f | cut -c12-)
+if [[ $result == $expresult ]];
+then
+    echo -e " \e[92mpassed"
+else
+    echo -e " \e[91mfailed"
+fi
+echo -e "\e[39m<<<================================>>>"
