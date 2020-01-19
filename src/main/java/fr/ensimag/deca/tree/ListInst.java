9ca947c5c3d1d37f7c27ffada6d48bf18bd0edc6
Dorian <Dorian@172.20.10.2>
2020-01-19 16:01:16 +0100

    fix decompilation

--- src/main/java/fr/ensimag/deca/tree/ListInst.java
+++ src/main/java/fr/ensimag/deca/tree/ListInst.java
@@ -47,7 +47,7 @@ public class ListInst extends TreeList<AbstractInst> {
     @Override
     public void decompile(IndentPrintStream s) {
         for (AbstractInst i : getList()) {
-            i.decompileInst(s);
+            i.decompile(s);
             s.println();
         }
     }
