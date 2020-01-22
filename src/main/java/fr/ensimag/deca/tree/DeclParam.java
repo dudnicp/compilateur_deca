77525e002f5a84fb1e3e3ca3a702a099bd60e825
Romain Pierre <romain.pierre@ensimag.fr>
2020-01-22 09:55:32 +0100

    AbstractDeclParam

--- src/main/java/fr/ensimag/deca/tree/DeclParam.java
+++ src/main/java/fr/ensimag/deca/tree/DeclParam.java
@@ -8,7 +8,7 @@ import fr.ensimag.deca.DecacCompiler;
 import fr.ensimag.deca.context.Type;
 import fr.ensimag.deca.tools.IndentPrintStream;
 
-public class DeclParam extends Tree {
+public class DeclParam extends AbstractDeclParam {
 
 	private AbstractIdentifier paramType;
 	
