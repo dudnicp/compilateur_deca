3444e404efda56d895f266b8d485a68c271ff2e4
Romain Pierre <romain.pierre@ensimag.fr>
2020-01-22 11:23:00 +0100

    amelioration syntaxe contexte objet passe 2

--- src/main/java/fr/ensimag/deca/tree/DeclMethod.java
+++ src/main/java/fr/ensimag/deca/tree/DeclMethod.java
@@ -5,6 +5,9 @@ import java.io.PrintStream;
 import fr.ensimag.deca.DecacCompiler;
 import fr.ensimag.deca.context.ClassDefinition;
 import fr.ensimag.deca.context.ContextualError;
+import fr.ensimag.deca.context.Definition;
+import fr.ensimag.deca.context.Environment.DoubleDefException;
+import fr.ensimag.deca.context.MethodDefinition;
 import fr.ensimag.deca.context.Signature;
 import fr.ensimag.deca.context.Type;
 import fr.ensimag.deca.tools.IndentPrintStream;
@@ -28,7 +31,15 @@ public class DeclMethod extends Tree {
 			Symbol currentClass) throws ContextualError {
 		Type verifiedType = type.verifyType(compiler);
 		Signature sig = listDeclParam.verifyListDeclParam(compiler);
-		
+		// TODO verifier la redifinition d'une methode
+		// ie meme returnType et signature
+		ClassDefinition classDef = (ClassDefinition)compiler.getEnvTypes().get(currentClass);
+		Symbol methodSymbol = classDef.getMembers().createSymbol(methodName.toString());
+		try {
+		classDef.getMembers().declare(methodSymbol, new MethodDefinition(verifiedType, this.getLocation(), sig, 0));
+		} catch (DoubleDefException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	
