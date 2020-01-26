d8646a7b444d0ccb717279d455a2cce721b04d24
Paul Dudnic <paul.dudnic@grenoble-inp.org>
2020-01-26 12:32:10 +0100

    codeGen fixing bugs for object, now seems to be working

--- src/main/java/fr/ensimag/deca/tree/AbstractLValue.java
+++ src/main/java/fr/ensimag/deca/tree/AbstractLValue.java
@@ -2,7 +2,10 @@ package fr.ensimag.deca.tree;
 
 import fr.ensimag.deca.context.Type;
 import fr.ensimag.ima.pseudocode.DAddr;
+import fr.ensimag.ima.pseudocode.IMAProgram;
+import fr.ensimag.ima.pseudocode.Register;
 import fr.ensimag.deca.DecacCompiler;
+import fr.ensimag.deca.codegen.RegisterManager;
 import fr.ensimag.deca.context.ClassDefinition;
 import fr.ensimag.deca.context.ContextualError;
 import fr.ensimag.deca.context.EnvironmentExp;
@@ -16,4 +19,6 @@ import fr.ensimag.deca.context.EnvironmentExp;
 public abstract class AbstractLValue extends AbstractExpr {
 
 	protected abstract DAddr daddr();	
+	
+	protected abstract DAddr tempAddr(IMAProgram program, int n, RegisterManager registerManager);
 }
